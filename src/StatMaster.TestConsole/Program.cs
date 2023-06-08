88b3a86946eb1147923796f4f10271e0c8ece395
Nikita Yudin <nikitayudin782@gmail.com>
2023-06-08 14:49:40 +0300

    feat: add test console

---
+++ src/StatMaster.TestConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using StatMaster;
+
+var health = new ModifiableValue<float>(100f);
+
+Console.WriteLine($"Health is {health.Value}.");
+
+// Output: Health is 100.
+
+health.Modifiers.Add(Modifier.Times(1.10f));
+
+Console.WriteLine($"Health is {health.Value}.");
+
+// Output: Health is 110.
+
+health.Modifiers.Add(Modifier.Plus(5f, "+5 health"));
+
+Console.WriteLine($"Health is {health.Value}.");
+
+// Output: Health is 115.
